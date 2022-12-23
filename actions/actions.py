b096558ef53ebefb925c8743e0f39f9c51134b60
Konstantinos Panayiotou <klpanagi@gmail.com>
2022-12-23 13:12:02 +0200

    Updates rasa actions

--- actions/actions.py
+++ actions/actions.py
@@ -33,58 +33,55 @@ def compute_user_properties(property):
     if property.lower() == 'address':
       return ''
 
-class Actionaskform12345formname(Action):
+
+class Validategiveweatherform(FormValidationAction):
 
     def name(self) -> Text:
-        return "action_ask_form12345_form_name"
+        return "validate_give_weather_form"
 
-    def run(self, dispatcher, tracker, domain):
-        output = []
-
-        dispatcher.utter_message(text = f"Whats your name?")
-
-        return output
-
-class Actionaskform12345formage(Action):
-
-    def name(self) -> Text:
-        return "action_ask_form12345_form_age"
-
-    def run(self, dispatcher, tracker, domain):
-        output = []
-
-        dispatcher.utter_message(text = f"How old are you?")
-
-        return output
-
-
-class Validateform12345form(FormValidationAction):
-
-    def name(self) -> Text:
-        return "validate_form12345_form"
-
-    def extract_age(self, dispatcher, tracker, domain):
+    def extract_temperature_answer(self, dispatcher, tracker, domain):
         output = {}
         requested_slot = tracker.get_slot('requested_slot')
-        if requested_slot == "age":
-            age = None
-            text = tracker.latest_message['text']
-            numbers = re.findall("\d+", text)
-            if len(numbers):
-                age = int(numbers[0])
-            output["age"] = age
+        if requested_slot == "temperature_answer":
+            temperature_answer = None
+            response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude=40.62&longitude=22.94&current_weather=true",
+                headers = {},
+                params = {}
+            )
+            try:
+                temperature_answer = response.json()['current_weather']['temperature']
+                output["temperature_answer"] = temperature_answer
+            except:
+                print(f'Error retrieving response from https://api.open-meteo.com/v1/forecast?latitude=40.62&longitude=22.94&current_weather=true with code {response.status_code}.')
         return output
 
-class Actionanswer(Action):
+    def extract_time_answer(self, dispatcher, tracker, domain):
+        output = {}
+        requested_slot = tracker.get_slot('requested_slot')
+        if requested_slot == "time_answer":
+            time_answer = None
+            response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude=40.62&longitude=22.94&current_weather=true",
+                headers = {},
+                params = {}
+            )
+            try:
+                time_answer = response.json()['current_weather']['time']
+                output["time_answer"] = time_answer
+            except:
+                print(f'Error retrieving response from https://api.open-meteo.com/v1/forecast?latitude=40.62&longitude=22.94&current_weather=true with code {response.status_code}.')
+        return output
+
+class Actionanswerback(Action):
 
     def name(self) -> Text:
-        return "action_answer"
+        return "action_answer_back"
 
     def run(self, dispatcher, tracker, domain):
-        name = tracker.get_slot('name')
+        time_answer = tracker.get_slot('time_answer')
+        temperature_answer = tracker.get_slot('temperature_answer')
         output = []
 
-        dispatcher.utter_message(text = f"Glad to meet you  { name }")
+        dispatcher.utter_message(text = f"The weather is  { temperature_answer } at { time_answer }")
 
         return output
 
