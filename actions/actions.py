0f4269e425041bf4653f3e9a300ca454bf1e4cdf
Konstantinos Panayiotou <klpanagi@gmail.com>
2022-12-23 13:24:15 +0200

    Updates rasa actions

--- actions/actions.py
+++ actions/actions.py
@@ -33,55 +33,15 @@ def compute_user_properties(property):
     if property.lower() == 'address':
       return ''
 
-
-class Validategiveweatherform(FormValidationAction):
-
-    def name(self) -> Text:
-        return "validate_give_weather_form"
-
-    def extract_temperature_answer(self, dispatcher, tracker, domain):
-        output = {}
-        requested_slot = tracker.get_slot('requested_slot')
-        if requested_slot == "temperature_answer":
-            temperature_answer = None
-            response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude=40.62&longitude=22.94&current_weather=true",
-                headers = {},
-                params = {}
-            )
-            try:
-                temperature_answer = response.json()['current_weather']['temperature']
-                output["temperature_answer"] = temperature_answer
-            except:
-                print(f'Error retrieving response from https://api.open-meteo.com/v1/forecast?latitude=40.62&longitude=22.94&current_weather=true with code {response.status_code}.')
-        return output
-
-    def extract_time_answer(self, dispatcher, tracker, domain):
-        output = {}
-        requested_slot = tracker.get_slot('requested_slot')
-        if requested_slot == "time_answer":
-            time_answer = None
-            response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude=40.62&longitude=22.94&current_weather=true",
-                headers = {},
-                params = {}
-            )
-            try:
-                time_answer = response.json()['current_weather']['time']
-                output["time_answer"] = time_answer
-            except:
-                print(f'Error retrieving response from https://api.open-meteo.com/v1/forecast?latitude=40.62&longitude=22.94&current_weather=true with code {response.status_code}.')
-        return output
-
-class Actionanswerback(Action):
+class Actionresp(Action):
 
     def name(self) -> Text:
-        return "action_answer_back"
+        return "action_resp"
 
     def run(self, dispatcher, tracker, domain):
-        time_answer = tracker.get_slot('time_answer')
-        temperature_answer = tracker.get_slot('temperature_answer')
         output = []
 
-        dispatcher.utter_message(text = f"The weather is  { temperature_answer } at { time_answer }")
+        dispatcher.utter_message(text = f"Hello friend")
 
         return output
 
