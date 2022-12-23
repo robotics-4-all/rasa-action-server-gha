0b2967a84ea7c35a7d7bf496df804c0519779ffd
Konstantinos Panayiotou <klpanagi@gmail.com>
2022-12-23 12:03:02 +0200

    Updates rasa actions

--- actions/actions.py
+++ actions/actions.py
@@ -33,152 +33,15 @@ def compute_user_properties(property):
     if property.lower() == 'address':
       return ''
 
-class Actionaskform12345formname(Action):
+class Actionresp(Action):
 
     def name(self) -> Text:
-        return "action_ask_form12345_form_name"
+        return "action_resp"
 
     def run(self, dispatcher, tracker, domain):
         output = []
 
-        dispatcher.utter_message(text = f"Whats your name?")
-
-        return output
-
-class Actionaskform12345formage(Action):
-
-    def name(self) -> Text:
-        return "action_ask_form12345_form_age"
-
-    def run(self, dispatcher, tracker, domain):
-        output = []
-
-        dispatcher.utter_message(text = f"How old are you?")
-
-        return output
-
-
-class Validateform12345form(FormValidationAction):
-
-    def name(self) -> Text:
-        return "validate_form12345_form"
-
-    def extract_age(self, dispatcher, tracker, domain):
-        output = {}
-        requested_slot = tracker.get_slot('requested_slot')
-        if requested_slot == "age":
-            age = None
-            text = tracker.latest_message['text']
-            numbers = re.findall("\d+", text)
-            if len(numbers):
-                age = int(numbers[0])
-            output["age"] = age
-        return output
-
-class Actionanswer(Action):
-
-    def name(self) -> Text:
-        return "action_answer"
-
-    def run(self, dispatcher, tracker, domain):
-        name = tracker.get_slot('name')
-        output = []
-
-        dispatcher.utter_message(text = f"Glad to meet you  { name }")
-
-        return output
-
-class Actionaskform1formcityslot(Action):
-
-    def name(self) -> Text:
-        return "action_ask_form1_form_city_slot"
-
-    def run(self, dispatcher, tracker, domain):
-        NAME = compute_user_properties("NAME")
-        output = []
-
-        dispatcher.utter_message(text = f"For which city { NAME } ?")
-
-        return output
-
-class Actionaskform1formtimeslot(Action):
-
-    def name(self) -> Text:
-        return "action_ask_form1_form_time_slot"
-
-    def run(self, dispatcher, tracker, domain):
-        output = []
-
-        dispatcher.utter_message(text = f"For when?")
-
-        return output
-
-
-class Validateform1form(FormValidationAction):
-
-    def name(self) -> Text:
-        return "validate_form1_form"
-
-    def extract_answer(self, dispatcher, tracker, domain):
-        output = {}
-        requested_slot = tracker.get_slot('requested_slot')
-        if requested_slot == "answer":
-            answer = None
-            time_slot = tracker.get_slot('time_slot')
-            city_slot = tracker.get_slot('city_slot')
-            NAME = compute_user_properties("NAME")
-            response = requests.get(f"r4a.issel.ee.auth.gr:8080/weather",
-                headers = {'city': f"{city_slot}", 'time': f"{time_slot}"},
-                params = {'city': [f"{city_slot}"], 'time': f"{time_slot}", 'user': f"{NAME}"}
-            )
-            try:
-                answer = response.json()['weather']['forecast']
-                output["answer"] = answer
-            except:
-                print(f'Error retrieving response from r4a.issel.ee.auth.gr:8080/weather with code {response.status_code}.')
-        return output
-
-class Actionanswerback(Action):
-
-    def name(self) -> Text:
-        return "action_answer_back"
-
-    def run(self, dispatcher, tracker, domain):
-        answer = tracker.get_slot('answer')
-        NAME = compute_user_properties("NAME")
-        CITY = compute_user_properties("CITY")
-        output = []
-
-        dispatcher.utter_message(text = f"Dear { NAME } the weather in { CITY } is { answer }")
-
-        ts = datetime.timestamp(datetime.now())
-        event_data = {
-            "id": "id",
-            "type": "event",
-            "timestamp": int(datetime.timestamp(datetime.now())),
-            "payload": {"message": {'a': '1', 'b': '2'}, "uri": f"/test" }
-        }
-        dispatcher.utter_message(json_message = event_data)
-
-        ts = datetime.timestamp(datetime.now())
-        event_data = {
-            "id": "id",
-            "type": "event",
-            "timestamp": int(datetime.timestamp(datetime.now())),
-            "payload": {"message": '/llalll', "uri": f"/test" }
-        }
-        dispatcher.utter_message(json_message = event_data)
-
-        ts = datetime.timestamp(datetime.now())
-        event_data = {
-            "id": "id",
-            "type": "event",
-            "timestamp": int(datetime.timestamp(datetime.now())),
-            "payload": {"message": ['a','1','b','2'], "uri": f"/test" }
-        }
-        dispatcher.utter_message(json_message = event_data)
-
-
+        dispatcher.utter_message(text = f"Hello friend")
 
         return output
 
