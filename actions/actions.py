bbd937a114471289a2b88631f71116fac747929d
Konstantinos Panayiotou <klpanagi@gmail.com>
2024-05-16 12:15:06 +0000

    Updates rasa actions

--- actions/actions.py
+++ actions/actions.py
@@ -58,10 +58,10 @@ class Actiongreetback(Action):
 
         return output
 
-class Actionaskaskstatusformformhowareyou(Action):
+class Actionaskform1formcityslot(Action):
 
     def name(self) -> Text:
-        return "action_ask_ask_status_form_form_how_are_you"
+        return "action_ask_form1_form_city_slot"
 
     def run(self, dispatcher, tracker, domain):
 
@@ -69,8 +69,51 @@ class Actionaskaskstatusformformhowareyou(Action):
 
 
          
-        dispatcher.utter_message(text = f"How are you?")
+        dispatcher.utter_message(text = f"For which city?")
 
-        output.append(SlotSet('how_are_you', None))
+        return output
+
+
+class Validateform1form(FormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_form1_form"
+
+    def extract_temp(self, dispatcher, tracker, domain):
+        output = {}
+        requested_slot = tracker.get_slot('requested_slot')
+        city_slot = tracker.get_slot('city_slot')
+        temp = tracker.get_slot('temp')
+        if city_slot != None and temp == None:
+            city_slot = tracker.get_slot('city_slot')
+            try:
+                response = requests.get(f"http://services.issel.ee.auth.gr/general_information/weather_openweather",
+                    headers = {},
+                    params = {'city': f"{city_slot}"}
+                )
+                temp = response.json()['temp']
+                output["temp"] = temp
+            except:
+                print(f'Error retrieving response from http://services.issel.ee.auth.gr/general_information/weather_openweather with code {response.status_code}.')
+                dispatcher.utter_message(text = "Apologies, something went wrong.")
+        return output
+
+class Actionanswerback(Action):
+
+    def name(self) -> Text:
+        return "action_answer_back"
+
+    def run(self, dispatcher, tracker, domain):
+
+        output = []
+
+        temp = tracker.get_slot('temp')
+        city_slot = tracker.get_slot('city_slot')
+
+         
+        dispatcher.utter_message(text = f"The weather forecast will be  { temp }  for  { city_slot }")
+
+        output.append(SlotSet('city_slot', None))
+        output.append(SlotSet('temp', None))
         return output
 
